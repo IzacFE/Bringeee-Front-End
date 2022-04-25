90a31074cad14d5e2f9483a26dbe90fab8ec7a21
unknown <izacaqsha@gmail.com>
2022-04-25 20:27:11 +0700

    order collection for drivers and admin is almost done

--- src/components/modalLogin/ModalLogin.jsx
+++ src/components/modalLogin/ModalLogin.jsx
@@ -2,17 +2,6 @@ import React, { useState } from "react";
 import { Modal, Button, Group, Input, InputWrapper } from "@mantine/core";
 
 const ModalLogin = (props) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleLogin = () => {
-    const data = {
-      yourEmail: email,
-      yourPass: password,
-    };
-    console.log(data);
-  };
-
   return (
     <>
       <Modal
@@ -28,13 +17,26 @@ const ModalLogin = (props) => {
         }}
       >
         <InputWrapper id="email" required label="Email">
-          <Input id="email" type="email" placeholder="Your email" onChange={(e) => setEmail(e.target.value)} />
+          <Input
+            id="email"
+            type="email"
+            placeholder="Your email"
+            onChange={props.email}
+          />
         </InputWrapper>
         <InputWrapper id="password" required label="Kata Sandi">
-          <Input id="password" type="password" placeholder="Your password" onChange={(e) => setPassword(e.target.value)} />
+          <Input
+            id="password"
+            type="password"
+            placeholder="Your password"
+            onChange={props.password}
+          />
         </InputWrapper>
         <Group position="right" className="my-5">
-          <Button className="bg-amber-500 hover:bg-amber-400 text-stone-700" onClick={() => handleLogin()}>
+          <Button
+            className="bg-amber-500 hover:bg-amber-400 text-stone-700"
+            onClick={props.login}
+          >
             Login
           </Button>
         </Group>
