b99de1ba388c8138da7d816134f3f36adc304e05
farhan <farhnn.r@gmail.com>
2022-04-25 19:22:36 +0700

    profilePage

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { ProfileCostumer, ProfileDriver } from "../../components/profile/ProfileUser";
+import { TabsProfileCostumer, TabsProfileDriver } from "../../components/tabsProfile/TabsProfile";
 
 function Profile() {
-  return <div>Profile</div>;
+  const [role, setRole] = useState("driver");
+
+  if (role === "costumer") {
+    return (
+      <div className="container mx-auto py-5">
+        <div className="flex flex-col md:flex-row md:gap-2">
+          <div className="w-full md:w-3/12">
+            <ProfileCostumer />
+          </div>
+          <div className="w-full md:w-9/12">
+            <TabsProfileCostumer />
+          </div>
+        </div>
+      </div>
+    );
+  } else if (role === "driver") {
+    return (
+      <div className="container mx-auto py-5">
+        <div className="flex flex-col md:flex-row md:gap-2">
+          <div className="w-full md:w-3/12">
+            <ProfileDriver />
+          </div>
+          <div className="w-full md:w-9/12">
+            <TabsProfileDriver />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Profile;
